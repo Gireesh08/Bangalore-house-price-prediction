2a7dbffc62a0e6c4fac76078ef00797973c3eb8f
Gireesh Adireddi <aadireddigireesh2003@gmail.com>
2026-05-25 01:23:35 +0530

    Added HTML serving routes

--- Server/Server.py
+++ Server/Server.py
@@ -1,8 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import util
 import os
 
 app = Flask(__name__)
+
+@app.route('/')
+def serve_index():
+    return send_from_directory('../Client', 'app.html')
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('../Client', filename)
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
